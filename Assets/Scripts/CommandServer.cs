826728da2de5d384bccfa7c1a20160778006ac57
demi180 <noamweiss@gmail.com>
2017-05-30 11:36:49 -0700

    Fixed memory leak from textures

--- Assets/Scripts/CommandServer.cs
+++ Assets/Scripts/CommandServer.cs
@@ -17,9 +17,9 @@ public class CommandServer : MonoBehaviour
 	public RawImage inset2;
 	public RawImage inset3;
 
-	Texture2D inset1Tex;
-	Texture2D inset2Tex;
-	Texture2D inset3Tex;
+//	Texture2D inset1Tex;
+//	Texture2D inset2Tex;
+//	Texture2D inset3Tex;
 
 	void Start()
 	{
@@ -32,9 +32,9 @@ public class CommandServer : MonoBehaviour
 //		_socket.On ( "get_samples", GetSamplePositions );
 		robotController = robotRemoteControl.robot;
 		frontFacingCamera = robotController.recordingCam;
-		inset1Tex = new Texture2D ( 1, 1 );
-		inset2Tex = new Texture2D ( 1, 1 );
-		inset3Tex = new Texture2D ( 1, 1 );
+//		inset1Tex = new Texture2D ( 1, 1 );
+//		inset2Tex = new Texture2D ( 1, 1 );
+//		inset3Tex = new Texture2D ( 1, 1 );
 	}
 
 	void OnOpen(SocketIOEvent obj)
@@ -63,7 +63,7 @@ public class CommandServer : MonoBehaviour
 		// try to load image1
 		bool loaded = false;
 		Vector2 size = Vector2.zero;
-		Texture2D tex = new Texture2D ( 1, 1 );
+		Texture2D tex = null;
 		string imageInfo = "";
 		byte[] imageBytes = null;
 		if ( jsonObject.HasField ( "inset_image1" ) )
@@ -71,24 +71,32 @@ public class CommandServer : MonoBehaviour
 		if ( !string.IsNullOrEmpty ( imageInfo ) )
 			imageBytes = Convert.FromBase64String ( imageInfo );
 		if ( imageBytes != null && imageBytes.Length != 0 )
+		{
+			tex = new Texture2D ( 1, 1 );
 			loaded = tex.LoadImage ( imageBytes, true );
+		}
 		if ( loaded && inset1 != null && tex.width > 1 && tex.height > 1 )
 		{
-			inset1Tex = tex;
-			inset1.texture = inset1Tex;
+			if ( inset1.texture != null )
+				Destroy ( inset1.texture );
+			inset1.texture = tex;
+//			inset1Tex = tex;
+//			inset1.texture = inset1Tex;
 			size = inset1.rectTransform.sizeDelta;
-			size.x = 1f * inset1Tex.width / inset1Tex.height * size.y;
+			size.x = 1f * tex.width / tex.height * size.y;
+//			size.x = 1f * inset1Tex.width / inset1Tex.height * size.y;
 			inset1.rectTransform.sizeDelta = size;
 			inset1.CrossFadeAlpha ( 1, 0.3f, false );
 		} else
 		if ( inset1 != null )
 		{
-//			if ( tex.width == 1 || tex.height == 1 )
-//				inset1.CrossFadeAlpha ( 0, 0.0f, true );
+//			tex = null;
 		}
 		// try to load image2
 		loaded = false;
-		tex = new Texture2D ( 1, 1 );
+//		if ( tex != null )
+//			Destroy ( tex );
+		tex = null;
 		imageInfo = "";
 		imageBytes = null;
 		if ( jsonObject.HasField ( "inset_image2" ) )
@@ -96,24 +104,33 @@ public class CommandServer : MonoBehaviour
 		if ( !string.IsNullOrEmpty ( imageInfo ) )
 			imageBytes = Convert.FromBase64String ( imageInfo );
 		if ( imageBytes != null && imageBytes.Length != 0 )
-			loaded = tex.LoadImage ( imageBytes, true );
-		if ( loaded && inset2Tex != null && tex.width > 1 && tex.height > 1 )
 		{
-			inset2Tex = tex;
-			inset2.texture = inset2Tex;
+			tex = new Texture2D ( 1, 1 );
+			loaded = tex.LoadImage ( imageBytes, true );
+		}
+		if ( loaded && inset2 != null && tex.width > 1 && tex.height > 1 )
+		{
+			if ( inset2.texture != null )
+				Destroy ( inset2.texture );
+			inset2.texture = tex;
+//			inset2Tex = tex;
+//			inset2.texture = inset2Tex;
 			size = inset2.rectTransform.sizeDelta;
-			size.x = 1f * inset2Tex.width / inset2Tex.height * size.y;
+			size.x = 1f * tex.width / tex.height * size.y;
+//			size.x = 1f * inset2Tex.width / inset2Tex.height * size.y;
 			inset2.rectTransform.sizeDelta = size;
 			inset2.CrossFadeAlpha ( 1, 0.3f, false );
 		} else
 		if ( inset2 != null )
 		{
-//			if ( tex.width == 1 || tex.height == 1 )
-//				inset2.CrossFadeAlpha ( 0, 0.0f, true );
+//			if ( inset1.texture != null )
+//				Destroy ( inset1.texture );
 		}
 		// try to load image3
 		loaded = false;
-		tex = new Texture2D ( 1, 1 );
+//		if ( tex != null )
+//			Destroy ( tex );
+		tex = null;
 		imageInfo = "";
 		imageBytes = null;
 		if ( jsonObject.HasField ( "inset_image3" ) )
@@ -121,21 +138,29 @@ public class CommandServer : MonoBehaviour
 		if ( !string.IsNullOrEmpty ( imageInfo ) )
 			imageBytes = Convert.FromBase64String ( imageInfo );
 		if ( imageBytes != null && imageBytes.Length != 0 )
-			loaded = tex.LoadImage ( imageBytes, true );
-		if ( loaded && inset3Tex != null && tex.width > 1 && tex.height > 1 )
 		{
-			inset3Tex = tex;
-			inset3.texture = inset3Tex;
+			tex = new Texture2D ( 1, 1 );
+			loaded = tex.LoadImage ( imageBytes, true );
+		}
+		if ( loaded && inset3 != null && tex.width > 1 && tex.height > 1 )
+		{
+			if ( inset3.texture != null )
+				Destroy ( inset3.texture );
+			inset3.texture = tex;
+//			inset3Tex = tex;
+//			inset3.texture = inset3Tex;
 			size = inset3.rectTransform.sizeDelta;
-			size.x = 1f * inset3Tex.width / inset3Tex.height * size.y;
+			size.x = 1f * tex.width / tex.height * size.y;
+//			size.x = 1f * inset3Tex.width / inset3Tex.height * size.y;
 			inset3.rectTransform.sizeDelta = size;
 			inset3.CrossFadeAlpha ( 1, 0.3f, false );
 		} else
 		if ( inset3 != null )
 		{
-//			if ( tex.width == 1 || tex.height == 1 )
-//				inset3.CrossFadeAlpha ( 0, 0.0f, true );
 		}
+//		if ( tex != null )
+//			Destroy ( tex );
+		tex = null;
 		EmitTelemetry(obj);
 	}
 
@@ -203,37 +228,4 @@ public class CommandServer : MonoBehaviour
 			_socket.Emit("telemetry", new JSONObject(data));
 		});
 	}
-
-/*	void GetSamplePositions (SocketIOEvent obj)
-	{
-		UnityMainThreadDispatcher.Instance ().Enqueue ( () =>
-		{
-			Debug.Log ("Sending sample positions");
-
-			Dictionary<string, string> data = new Dictionary<string, string> ();
-
-			int count = ObjectiveSpawner.samples.Length;
-			data[ "sample_count" ] = count.ToString ();
-			string x = "";
-			string y = "";
-
-			for (int i = 0; i < count; i++ )
-			{
-				GameObject go = ObjectiveSpawner.samples[i];
-				x += go.transform.position.x.ToString ("N2") + ",";
-				y += go.transform.position.z.ToString ("N2") + ",";
-//				data[ "sample" + i + "_pos_x" ] = go.transform.position.x.ToString ("N4");
-//				data[ "sample" + i + "_pos_y" ] = go.transform.position.z.ToString ("N4");
-			}
-			x = x.TrimEnd (',');
-			y = y.TrimEnd (',');
-			data["samples_x"] = x;
-			data["samples_y"] = y;
-//			Debug.Log (x + "\n" + y);
-
-//			foreach (KeyValuePair<string, string> pair in data)
-//				Debug.Log ("key: " + pair.Key + " value: " + pair.Value + "\n");
-			_socket.Emit ("sample_positions", new JSONObject(data));
-		} );
-	}*/
 }
