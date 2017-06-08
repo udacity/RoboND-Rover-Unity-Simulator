451e4a9efbdf4988cd2d747aaa7772e6d55119f2
demi180 <noamweiss@gmail.com>
2017-06-07 18:53:00 -0700

    Rotation demo - added UI and made more modular. Added camera pan/rotate

---
+++ Assets/Scripts/UIRotationList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRotationList : MonoBehaviour
+{
+	public UIRotationOption prefab;
+	public List<UIRotationOption> rotations = new List<UIRotationOption> ();
+
+	public void AddRotation (Transform t)
+	{
+		UIRotationOption inst = Instantiate ( prefab, transform );
+		inst.transform.SetSiblingIndex ( t.GetSiblingIndex () + 1 );
+		rotations.Add ( inst );
+		inst.gameObject.SetActive ( true );
+	}
+
+	public void AddRotationBefore (Transform t)
+	{
+		UIRotationOption inst = Instantiate ( prefab, transform );
+		inst.transform.SetSiblingIndex ( t.GetSiblingIndex () - 1 );
+		rotations.Add ( inst );
+		inst.gameObject.SetActive ( true );
+	}
+
+	public void RemoveRotation (UIRotationOption option)
+	{
+		rotations.Remove ( option );
+		Destroy ( option.gameObject );
+	}
+}
